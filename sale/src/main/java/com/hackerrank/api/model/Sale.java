a0fce7272119c8eb0df10f385e6f9d6fc7ebe98f
Santhosh1l <e.g.santhosh2004@gmail.com>
2026-02-15 18:48:48 +0530

    first commit

---
+++ sale/src/main/java/com/hackerrank/api/model/Sale.java
@@ -0,0 +1,40 @@
+package com.hackerrank.api.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Negative;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Entity
+public class Sale implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message="Product name is mandatory")
+    private String productName;
+
+    @Email(message="Invalid customer email")
+    private String customerEmail;
+
+    @Positive(message = "Value should not be negative")
+    private Integer buyingPrice;
+
+    @Positive(message = "Value should not be negative")
+    private Integer sellingPrice;
+}
